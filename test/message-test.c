f3f4edd2714d316961f4ba8080810ab2240505b4
Stephen Ayotte <stephen.ayotte@gmail.com>
2013-08-18 19:45:01 -0400

    Add'l unit test for message.c

--- test/message-test.c
+++ test/message-test.c
@@ -5,6 +5,42 @@
 #include "test.h"
 #include "message.h"
 
+#define MAC_OS_X
+#ifdef MAC_OS_X
+#include <malloc/malloc.h> /* malloc_size() on Mac */
+int extend_string_allocates_unitsize_blocks()
+{
+    char    *dst;
+    char    *src;
+    int     dstlen;
+    int     unitsize, before, after;
+
+
+    unitsize = malloc_good_size(1);
+    dst = malloc(unitsize);
+    memset(dst, 'a', unitsize);
+    dst[unitsize - 1] = '\0';
+    dstlen = unitsize - 1;
+
+    src = malloc(unitsize * 2);
+    memset(src, 'b', unitsize * 2);
+    src[(unitsize * 2) - 1] = '\0';
+
+    fork_to_test(
+        before = malloc_size(dst);
+        extend_string(&dst, &dstlen, src, strlen(src), unitsize);
+        after = malloc_size(dst);
+//        printf("\nunitsize: %d\tbefore: %d\tafter: %d\n", unitsize, before, after);
+//        puts(dst);
+        if(after <= before)
+            exit(1);
+        if((after % unitsize) != 0)
+            exit(2);
+        exit(0);
+    )
+}
+#endif
+
 int extend_string_allocates_memory_given_null_ptr()
 {
     char    *dst;
@@ -32,6 +68,7 @@ int main()
     fail = 0;
 
     returns_zero(count, fail, "extend_string() allocates memory, given a NULL ptr", extend_string_allocates_memory_given_null_ptr);
+    returns_zero(count, fail, "extend_string() allocates memory in the specified unit size", extend_string_allocates_unitsize_blocks);
 
     printf("\n[message-test] Total tests passed/run: %d/%d\n", count - fail, count);
     puts("------------------------------------------------------------\n");
